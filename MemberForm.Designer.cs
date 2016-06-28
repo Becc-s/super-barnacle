7876881947e3ba38d0bc43e48ca78f5fcdc3bec6
Becc-s <rebecka.a-h@hotmail.com>
2016-06-28 12:54:49 +0000

    Add files via upload

---
+++ MemberForm.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Library
+{
+    partial class MemberForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPId = new System.Windows.Forms.TextBox();
+            this.txnName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAddMember = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtPId
+            // 
+            this.txtPId.Location = new System.Drawing.Point(12, 101);
+            this.txtPId.Name = "txtPId";
+            this.txtPId.Size = new System.Drawing.Size(120, 20);
+            this.txtPId.TabIndex = 0;
+            // 
+            // txnName
+            // 
+            this.txnName.Location = new System.Drawing.Point(12, 36);
+            this.txnName.Name = "txnName";
+            this.txnName.Size = new System.Drawing.Size(120, 20);
+            this.txnName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Personal ID";
+            // 
+            // btnAddMember
+            // 
+            this.btnAddMember.Location = new System.Drawing.Point(15, 162);
+            this.btnAddMember.Name = "btnAddMember";
+            this.btnAddMember.Size = new System.Drawing.Size(75, 23);
+            this.btnAddMember.TabIndex = 4;
+            this.btnAddMember.Text = "Add member";
+            this.btnAddMember.UseVisualStyleBackColor = true;
+            this.btnAddMember.Click += new System.EventHandler(this.btnAddMember_Click);
+            // 
+            // MemberForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnAddMember);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txnName);
+            this.Controls.Add(this.txtPId);
+            this.Name = "MemberForm";
+            this.Text = "MemberForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPId;
+        private System.Windows.Forms.TextBox txnName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAddMember;
+    }
+}
